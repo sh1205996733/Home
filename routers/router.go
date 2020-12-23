7781e701ab13926aa7909cc408c6beb6de77ded7
=shangyc <=15038311053@163.com>
2020-12-23 10:58:42 +0800

    增加登录

--- routers/router.go
+++ routers/router.go
@@ -6,9 +6,12 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
+	/**地区列表*/
 	beego.Router("/api/v1.0/areas", &controllers.AreaController{})
+	/**Session处理*/
 	beego.Router("/api/v1.0/session", &controllers.SessionController{})
+	/**房屋首页*/
 	beego.Router("/api/v1.0/houses/index", &controllers.HouseIndexController{})
+	/**用户登录*/
 	beego.Router("/api/v1.0/users", &controllers.UserController{})
 }
